87b9f6af7e3a3aa2489e7452053416c8e6c01766
Andrew Penny <103720353+A-Penny@users.noreply.github.com>
2023-02-18 22:47:40 -0600

    added calendar and scheduling functionality

--- frontend/src/components/Schedule.js
+++ frontend/src/components/Schedule.js
@@ -6,12 +6,27 @@ import 'react-calendar/dist/Calendar.css';
 
 export default function Schedule() {
     const [value, onChange] = React.useState(new Date());
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    
+    function onClickDay(value, event) {
+        onChange(value);
+         
+    }
+    let month = months[value.getMonth()]
     
         return (
             <div className='schedule-container'>
                 <h1>Upcoming Hikes</h1>
-                <Calendar onChange={onChange} value={value}/>
+                <Calendar 
+                    onChange={onChange} 
+                    value={value}
+                    calendarType='US'
+                    onClickDay={onClickDay}
+                    tileContent={({ activeStartDate, date, view }) => view === 'month' && date.getDate() === 19 && date.getMonth() === 2 ? <p>Church!</p> : null}
+                    />
+                <p>What's happening on {month} {value.getDate()}?</p>
             </div>
+            
             
         )
     }
