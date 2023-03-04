8924db651a860f5241260e322ef64224b6e8cdf6
Andrew Penny <103720353+A-Penny@users.noreply.github.com>
2023-03-04 09:13:49 -0600

    fixed map issue and added links to park list

--- frontend/src/components/HikingFacts.js
+++ frontend/src/components/HikingFacts.js
@@ -2,30 +2,19 @@ import React from "react";
 import '../App.css';
 
 
+
 export default function HikingFacts() {
-    const [parks, setParks] = React.useState([]);
+    const [parks, setParks] = React.useState(null)  
 
     React.useEffect(() => {
-        //fetch data
-        const dataFetch = async () => {
-          const data = await (
-            await fetch(
-              "https://services5.arcgis.com/bPacKTm9cauMXVfn/arcgis/rest/services/TN_State_Parks_Points/FeatureServer/0/query?where=1%3D1&outFields=PARK_NAME,TSP_UID,AM_FIRST_NAME,AM_LAST_NAME,AM_TITLE,AM_PHONE,PM_FIRST_NAME,PM_LAST_NAME,PM_TITLE,PM_PHONE&outSR=4326&f=json"
-            )
-          ).json();
-            
-          // set state when the data received
-          setParks(data);
-        };
-    
-         dataFetch();
-      }, []);
-      
-      
-      const parksArray = parks.features.map(park => {
-        const parkName = park.attributes['PARK_NAME'];
-        return <p>{parkName}</p>
-      });
+      fetch("https://services5.arcgis.com/bPacKTm9cauMXVfn/arcgis/rest/services/TN_State_Parks_Points/FeatureServer/0/query?where=1%3D1&outFields=PARK_NAME,TSP_UID,AM_FIRST_NAME,AM_LAST_NAME,AM_TITLE,AM_PHONE,PM_FIRST_NAME,PM_LAST_NAME,PM_TITLE,PM_PHONE&outSR=4326&f=json")
+      .then(response => {
+        return response.json()
+      })  
+      .then(data => {
+          setParks(data.features);
+        })
+    }, []);
       
     return (
         <div className="facts-container">
@@ -37,7 +26,10 @@ export default function HikingFacts() {
                 <li>Nature is beautiful</li>
             </ul>
             <p>Parks:</p>
-            {parksArray}
+            <div className="park-list-container">
+            {parks && parks.map(park => {return (<a href={`https://tnstateparks.com/parks/${park.attributes.PARK_NAME.replace(/\s+/g, '-').toLowerCase().replace('-state-park', '')}`} target='_blank'><p className='park-list'>{park.attributes.PARK_NAME}</p></a>)})}
+            </div>
+           
         </div>
         
     )
